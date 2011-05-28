cdc5b1ba8dc0c40d675cc65443d097b0fc1690f7
Marty Schoch <marty.schoch@gmail.com>
2011-05-28 13:14:47 -0400

    modified to delete files individually and leave main folder alone

--- MobileCouchbase/Scripts/download_couchbase.sh
+++ MobileCouchbase/Scripts/download_couchbase.sh
@@ -5,10 +5,10 @@ buildAction () {
 
 echo "Building..."
 
+mkdir -p "${TARGET_TEMP_DIR}"
 cd "${TARGET_TEMP_DIR}"
 echo "Downloading file to ${TARGET_TEMP_DIR}"
 `curl -O "http://hexeditreality.com/sites/default/files/iOSCouchbase.zip"`
-mkdir -p "${PROJECT_DIR}/Products/"
 cd "${PROJECT_DIR}/Products"
 echo "Extracting files to ${PROJECT_DIR}/Products"
 unzip  "${TARGET_TEMP_DIR}/iOSCouchbase.zip"
@@ -19,8 +19,12 @@ echo "Done..."
 
 cleanAction () {
 echo "Cleaning..."
-rm -rvf "${PROJECT_DIR}/Products/"
-mkdir -p "${PROJECT_DIR}/Products/"
+rm -rvf "${PROJECT_DIR}/Products/Couchbase.bundle"
+rm -rvf "${PROJECT_DIR}/Products/Couchbase.h"
+rm -rvf "${PROJECT_DIR}/Products/libCouchbase-iphoneos.a"
+rm -rvf "${PROJECT_DIR}/Products/libCouchbase-iphonesimulator.a"
+rm -rvf "${PROJECT_DIR}/Products/libTouchJSON-iphoneos.a"
+rm -rvf "${PROJECT_DIR}/Products/libTouchJSON-iphonesimulator.a"
 echo "Done..."
 }
 
